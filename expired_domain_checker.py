b366c0c31eec80560daaaa973a77d06cdabb57d9
Brendan McDevitt <bpmcdevitt@gmail.com>
2017-08-03 14:44:03 -0400

    made ipwhois function to do whois lookup for ip address

--- expired_domain_checker.py
+++ expired_domain_checker.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python3
 # do whoislookups from a list of domain names and output expired or not
 
-import sys
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("whois.apnic.net", 43))
-s.send(sys.argv[1] + "\r\n")
+def ipwhois(ip):
+	
 
-response = ""
-while True:
-    data = s.recv(4096)
-    response += data
-    if not data:
-        break
-s.close()
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect(("whois.apnic.net", 43))
+	s.send(ip + "\r\n")
 
-print response
+	response = ""
+	while True:
+	    data = s.recv(4096)
+	    response += data
+	    if not data:
+	        break
+	s.close()
+
+	print response
+
