048113e863e5ee40157521862f804be3259c70ad
Brendan McDevitt <bpmcdevitt@gmail.com>
2017-08-03 02:12:08 -0500

    added caesar cipher encrypt / decrypt scripts

---
+++ caesar_decrypt.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# this will solve a caesar cipher with the corresponding shift number
+# uses python built ins ord and char to convert ascii text to ordinal number then subtracts shifted number from ordinal 
+import sys
+
+msg = str(sys.argv[1])
+msg_length = len(msg)
+shift_amt = int(sys.argv[2])
+
+def get_ord(msg):
+
+    ord_msg_list = []
+
+    for letter in msg:
+        ord_msg_list.append(ord(letter))
+    return ord_msg_list
+
+def subtract_shift(shift_amt):
+    
+    subtract_shift_list = []
+
+    for ordinal in get_ord(msg):
+      subtract_shift_list.append(chr(ordinal - shift_amt))
+    return subtract_shift_list
+
+print(*subtract_shift(shift_amt), sep='', end='\n', flush=True)
