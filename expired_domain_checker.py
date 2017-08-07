366402321ef683fcd0e5a24e0d40e66c18b7cb2b
Brendan McDevitt <bpmcdevitt@gmail.com>
2017-08-07 13:11:00 -0400

    added domain ip lookup method

--- expired_domain_checker.py
+++ expired_domain_checker.py
@@ -5,11 +5,14 @@ import socket
 
 class Whois(object):
 	''' The Whois class which will handle all whois lookups via python'''
-
+	
+	def __init__(self):
+		pass
+	
 	def iplookup(self, ip):
 
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	
-		s.connect(("whois.apnic.net", 43))
+		s.connect(("whois.arin.net", 43))
 		s.send(ip + "\r\n")
 
 		response = ""
@@ -22,3 +25,17 @@ class Whois(object):
 
 		return response
 
+	def domainlookup(self, domain):
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect(("whois.verisign-grs.com", 43))
+		s.send(ip + "\r\n")
+
+		response = ""
+		while True:
+			data = s.recv(4096)
+			response += data
+			if not data:
+				break
+		s.close()
+
+		return response
