8dc9625dd7eb825855e0718649b7d0411e33e362
Brendan McDevitt <bpmcdevitt@thelinuxspace.com>
2017-08-23 12:16:57 -0500

    hacking together crap

--- atbash.py
+++ atbash.py
@@ -6,8 +6,15 @@ import sys
 
 reversed_xyz = {'A': 'Z','B':'Y','C':'X','D':'W', 'E':'V', 'F':'U','G':'T','H':'S','I':'R','J':'Q','K':'P','L':'O','M':'N','N':'M','O':'L','P':'K','Q':'J','R':'I','S':'H','T':'G','U':'F','V':'E','W':'D','X':'C','Y':'B','Z':'A'}
 
-def encrypt_msg(msg):
-    pass
 
+def encrypt_msg(msg):
+    msg_l = []
+    for letter in msg:
+        msg_l.append(letter)
+    
+    for i in range(len(msg_l)):
+        
 def decrypt_msg(msg):
     pass
+
+encrypt_msg('test')
