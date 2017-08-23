22827882fe10408baaf4fb3a8e7bfdab162b922b
Brendan McDevitt <bpmcdevitt@thelinuxspace.com>
2017-08-23 12:04:39 -0500

    added atbash

--- atbash.py
+++ atbash.py
@@ -6,9 +6,6 @@ import sys
 
 reversed_xyz = {'A': 'Z','B':'Y','C':'X','D':'W', 'E':'V', 'F':'U','G':'T','H':'S','I':'R','J':'Q','K':'P','L':'O','M':'N','N':'M','O':'L','P':'K','Q':'J','R':'I','S':'H','T':'G','U':'F','V':'E','W':'D','X':'C','Y':'B','Z':'A'}
 
-print(reversed_xyz.keys())
-print(reversed_xyz.values())
-
 def encrypt_msg(msg):
     pass
 
